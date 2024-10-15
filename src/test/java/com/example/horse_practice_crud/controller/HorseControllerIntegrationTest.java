d32edc958cd9bc677c3b53eb7f8d0e2d4930d9c3
Alissa Reynolds <144733508+alissareynolds@users.noreply.github.com>
2024-10-15 13:06:05 -0700

    added integration test to create method in controller

---
+++ src/test/java/com/example/horse_practice_crud/controller/HorseControllerIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.example.horse_practice_crud.controller;
+
+import com.example.horse_practice_crud.model.Horse;
+import com.example.horse_practice_crud.service.HorseService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HorseControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private HorseService mockHorseService;
+
+    private final Horse horse = new Horse(UUID.fromString("59c47568-fde0-4dd7-9aef-03db6a962810"), "Whisper", "black", "gelding", "mustang", 5, true);
+
+    @Test
+    public void createHorse() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .post("/api/horses")
+                .content(asJsonString(new Horse(null, "Whisper", "black", "gelding", "mustang", 5, true)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
